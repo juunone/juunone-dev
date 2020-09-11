bc54b66aa6e23de7c9efb7dd3cc28a215093565e
juunone <juunone@gmail.com>
2020-09-11 14:34:31 +0900

    Gatsby 블로그 이전

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `Juunone dev`,
+  description: `개발 폐관수련`,
+  author: `juunone`,
+  introduction: `FE`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `juunone`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `juunone`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`blog`, `dev1`, `fe`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'https-juunone-github-io', // Your disqus-short-name. check disqus.com.
+    utterances: 'juuone/juunone-dev', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'juunone',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-131472455-1', // Add your google analytics tranking ID
 }
