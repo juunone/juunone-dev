ea8a66dd6648ba4cfe6447d86017b7a014f21260
juunone <juunone@gmail.com>
2022-01-21 14:39:07 +0900

    feat: 애드센스 교체

--- src/html.js
+++ src/html.js
@@ -12,6 +12,8 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7767674261514377"
+     crossorigin="anonymous"></script>
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
